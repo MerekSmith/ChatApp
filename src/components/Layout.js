6eaa2d0b525d32931bf24ae1b5258ffca8b08870
Merek Smith <mereksmith@hotmail.com>
2019-07-11 16:44:27 -0600

    fixed layout socketURL

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { USER_CONNECTED, LOGOUT, VERIFY_USER } from "../Events";
 import LoginForm from "./LoginForm";
 import ChatContainer from "./chats/ChatContainer";
 
-const socketUrl = "/test";
+const socketUrl = "/";
 export default class Layout extends Component {
   constructor(props) {
     super(props);
